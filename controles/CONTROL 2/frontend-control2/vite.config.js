b411e655f693b0d0aba934b158fe16d96c62c8a6
emiliopobleteleiva <emilio.poblete@usach.cl>
2026-05-19 16:09:57 -0400

    se crean Dockerfile para front y backend y docker-compose para ambos
    se modifica application properties y vite.config.js para dara a la pagina de login

--- controles/CONTROL 2/frontend-control2/vite.config.js
+++ controles/CONTROL 2/frontend-control2/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    host: true, // necesario para que Docker pueda acceder
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080", // en local; en Docker se sobreescribe con env
+        changeOrigin: true,
+      },
+    },
+  },
+});
