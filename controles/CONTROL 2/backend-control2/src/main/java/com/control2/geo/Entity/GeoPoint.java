414dd29af957ef86cae18128c29eda9e79f80176
zero <jesus.ganga@usach.cl>
2026-05-23 00:26:29 -0400

    Fixsing geopoints view in front end

--- controles/CONTROL 2/backend-control2/src/main/java/com/control2/geo/Entity/GeoPoint.java
+++ controles/CONTROL 2/backend-control2/src/main/java/com/control2/geo/Entity/GeoPoint.java
@@ -11,6 +11,8 @@ import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Data
 @NoArgsConstructor
@@ -20,14 +22,29 @@ public class GeoPoint {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
-    private Long idGeoPoint; 
-    
+    private Long idGeoPoint;
+
     @Column(nullable = false)
     private String name;
 
+    @JsonIgnore
     @Column(nullable = false, columnDefinition = "geometry(Point,4326)")
     private Point point;
 
     @Column(nullable = false)
     private String sector;
+
+    // Metodos virtuales, que hacen? recogen la informacion del objeto point que
+    // tenemos
+    // guardado aqui mismo pero con cosas que un json puede leer como latitud y y
+    // longitud x
+    @JsonProperty("latitude")
+    public Double getLatitude() {
+        return point != null ? point.getY() : null;
+    }
+
+    @JsonProperty("longitude")
+    public Double getLongitude() {
+        return point != null ? point.getX() : null;
+    }
 }
