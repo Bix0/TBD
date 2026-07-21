e20e2e858d39da48799d4389db1cc0b16483b9dc
Alejandra Silva <alejandra.silva.sa@usach.cl>
2026-07-21 04:29:59 -0400

    vizualizacion ranking actualizado

--- laboratorios/Lab2/backend/mmorpg/src/main/java/com/grupo3/mmorpg/MmorpgApplication.java
+++ laboratorios/Lab2/backend/mmorpg/src/main/java/com/grupo3/mmorpg/MmorpgApplication.java
@@ -2,12 +2,12 @@ package com.grupo3.mmorpg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MmorpgApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MmorpgApplication.class, args);
 	}
-
 }
